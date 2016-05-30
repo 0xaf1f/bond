c05d2e17808aba72260a1839f538484c6380f58b
Afif Elghraoui <afif@ghraoui.name>
2016-05-29 19:22:24 -0700

    Use a main function within bondcmd.pl
    This allows writing separate functions while still keeping
    the main code at the top of the file.
    A function will be needed for what I'm about to do next.

--- bondcmd.pl
+++ bondcmd.pl
@@ -3,34 +3,45 @@
 use strict;
 use warnings;
 
+sub main {
+    my $prefix = $ARGV[0];
 
-my $prefix = $ARGV[0];
-#my $precedence = $ARGV[1];
+    my %variables = (
+	PATH    => ['sbin', 'bin'],
+	MANPATH => ['share/man'],
+	LIBRARY_PATH => ['lib'],
+	LD_LIBRARY_PATH => ['lib'],
+	CPATH => ['include'],
+	PERL5LIB => ['lib/perl5/site_perl'],
+	PYTHONPATH => ['lib/python2.7/site-packages'],
+	R_LIBS => ['lib/R/site-library'],
+	);
 
-my %variables = (
-    PATH    => ['sbin', 'bin'],
-    MANPATH => ['share/man'],
-    LIBRARY_PATH => ['lib'],
-    LD_LIBRARY_PATH => ['lib'],
-    CPATH => ['include'],
-    PERL5LIB => ['lib/perl5/site_perl'],
-    PYTHONPATH => ['lib/python2.7/site-packages'],
-    R_LIBS => ['lib/R/site-library'],
-    );
+    my $env_command;
 
-my $env_command = '';
+    foreach my $var (keys %variables) {
+	# Initialize empty variables to have a single colon.
+	# This is important for variables like MANPATH, which use the extra
+	# colon as a placeholder for the default search path.
+	$ENV{$var} = ':' if !defined $ENV{$var};
 
-foreach my $var (keys %variables) {
-    # Initialize empty variables to have a single colon.
-    # This is important for variables like MANPATH, which use the extra colon
-    # as a placeholder for the default search path.
-    $ENV{$var} = ':' if !defined $ENV{$var};
+	foreach my $element (@{$variables{$var}}) {
+	    $ENV{$var} = "$prefix/$element:$ENV{$var}"
+	}
 
-    foreach my $element (@{$variables{$var}}) {
-	$ENV{$var} = "$prefix/$element:$ENV{$var}"
+	$env_command .= generate_export_cmd($var, $ENV{$var});
     }
 
-    $env_command .= "export $var=\"$ENV{$var}\"\n";
+    print $env_command;
 }
 
-print $env_command;
+sub generate_export_cmd {
+    my (
+	$variable,
+	$value,
+	) = @_;
+
+    return "export $variable=\"$value\"\n";
+}
+
+exit(main(@ARGV));
